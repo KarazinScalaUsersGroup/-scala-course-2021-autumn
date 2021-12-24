671ee69b4693e6a8200aa07e7e1bbbb33d546d7e
myDiamondsDancing <66516985+dmitriyKolmogorov@users.noreply.github.com>
2021-12-24 22:31:43 +0200

    Update Homework.scala

--- src/main/scala/karazin/scala/users/group/week3/Homework.scala
+++ src/main/scala/karazin/scala/users/group/week3/Homework.scala
@@ -1,12 +1,13 @@
 package karazin.scala.users.group.week3
 
-object Homework:
+import scala.annotation.tailrec
+
+object Homework {
   
   // Peano numbers
   abstract class Nat:
     def isZero: Boolean
     def predecessor: Nat
-    def successor: Nat = ???
     
     infix def + (that: Nat): Nat
     
@@ -16,35 +17,99 @@ object Homework:
     def toInt: Int
     
     // Optional task
-    def fromInt(int: Int) = ???
+    def fromInt(i: Int): Nat = {
+
+      if (i < 0)
+        throw new Exception("Can not convert from negative number")
+
+      @tailrec
+      def inner(i: Int, to_return: Nat): Nat = i match {
+        case 0 => to_return
+        case p => inner(i - 1, Succ(to_return))
+      }
+
+      inner(i, Zero)
+    }
   
-    override def toString: String = s"Nat($predecessor)"
+    override def toString: String = s"Succ($predecessor)"
   
   type Zero = Zero.type 
-  object Zero extends Nat:
+  object Zero extends Nat {
+
+    // zero is zero
     def isZero: Boolean = true
     def predecessor: Nat = throw new Exception("0 doesn't have a predecessor")
     
-    infix def +(that: Nat): Nat = ???
+    // zero + anything = anything
+    infix def +(that: Nat): Nat = that
     
-    infix def -(that: Nat): Nat = ???
+    // we can not subtract from zero in the field of natural numbers
+    infix def -(that: Nat): Nat = that match {
+      case _:Zero => Zero
+      case _ => throw new Exception("Nat can not be negative (only Zero or Succ)")
+    }
     
-    // Optional task
-    def toInt: Int = ???
+    // this class representes zero
+    def toInt: Int = 0
 
     override def toString: String = "Zero"
-    override def equals(obj: Any): Boolean = ???
 
-  class Succ(n: Nat) extends Nat:
+    // since we represent number in this class
+    // we can only compare it with number types, Zero and Succ
+    override def equals(obj: Any): Boolean = obj match {
+      // zero = zero
+      case _:Zero => true
+
+      // this is also case for Succ
+      case _ => false
+    }
+  }
+
+  class Succ(n: Nat) extends Nat {
+
+    // Succ instance represents positive number
     def isZero: Boolean = false
-    def predecessor: Nat = ???
+    def predecessor: Nat = n
     
-    infix def +(that: Nat): Nat = ???
+    infix def +(that: Nat): Nat = that match {
+      case z: Zero => this
+      case _       => Succ(this) + that.predecessor
+    }
     
-    infix def -(that: Nat): Nat = ???
-    
-    // Optional task
-    def toInt: Int = ???
+    infix def -(that: Nat): Nat = that match {
+      // anything - zero = anything
+      case _:Zero => this
 
-    override def equals(obj: Any): Boolean = ???
+      case _ => this.predecessor - that.predecessor
+    }
+    
+    // recursive computing
+    def toInt: Int = {
+      
+      @tailrec
+      def inner(to_return: Int, predecessor: Nat): Int = predecessor match {
+        case p: Zero => to_return + 1
+        case p: Succ => inner(to_return + 1, p.predecessor)
+      }
 
+      inner(0, this.predecessor)
+    }
+
+    // since we represent number in this class
+    // we can only compare it with number types, Zero and Succ
+    override def equals(obj: Any): Boolean = obj match {
+      // recursive comparation
+      case p: Succ => this.predecessor == p.predecessor
+
+      // this is also case for Zero
+      case _ => false
+    }
+  }
+
+  @main
+  def main(): Unit = {
+    val num1 = Succ(Succ(Succ(Zero)))
+
+    print(num1 + Succ(Succ(Zero)))
+  }
+}
